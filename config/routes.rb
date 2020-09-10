b4e20d34399728845fdfe0006681d2e2afa468b2
Giju Ambrose <giju@techversantinfo.com>
2020-09-10 12:51:17 +0530

    Handled wrong urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   scope "(:locale)" do
     root 'user_messages#index'
     resources :user_messages, only: [:create]
+    get '/contact', to: 'user_messages#index'
+    get '*path' => redirect('/')
   end
+
 end
