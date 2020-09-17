18b567ee5e9858690a336ea49f5f8f2f4c098816
Giju Ambrose <giju@techversantinfo.com>
2020-09-17 16:58:00 +0530

    Added validations for usermessage model

--- app/models/user_message.rb
+++ app/models/user_message.rb
@@ -1,4 +1,7 @@
 class UserMessage < ApplicationRecord
-  validates :first_name, :message, :last_name, :email, :phone_number, presence: true 
+  validates :first_name, :message, :last_name, :email, :phone_number, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :first_name, uniqueness: { scope: :first_name }
+  validates_format_of :phone_number, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
 end
 
